f923a53252fb45ab8958ff8a7a171eb9abda41b1
Abhishek <u6411540@anu.edu.au>
2019-09-30 16:49:46 -0400

    Plot Added

--- runmanycode.sh
+++ runmanycode.sh
@@ -1,17 +1,26 @@
 ##IMPORTANT : Run the code using the Bash command not the sh command .So to run the code do the following: bash runmanycode.sh
 ##This is a shell script to run multiple instance of K and n 
-##Step 1: Create the necessary folder as required by the code 
+
+##Step 1: Create the necessary folder as required by the code (and delete if alrady exists )
 ##Step 1 : Create the folder where the output is going to be : 
 ##Step2 : Remove the replication folder if already exists 
 ##Step 3 : Run the python Script
 
 
-for ((K=305;K<=305;K=K+5))
+for ((K=01;K<=1;K=K+5))
 do
 if [ $K -eq 0 ]
    then $K = $K+1
 fi
 ##Step1
+##Deleting the file if it exists already : 
+DIR=/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5
+if [ -d "$DIR" ]; then
+    echo "Removing Lock"
+    rm -rf "$DIR"
+fi
+
+##Create the necessary folder 
   mkdir "/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5"
   mkdir "/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5/weight_transfer"
  touch "/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5/weight_transfer/opts.txt"
@@ -40,26 +49,17 @@ weight_transfer
 " >> "/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5/weight_transfer/opts.txt"
 
 
+##Step 2 : 
   mkdir "/home/abhishek/Desktop/V2_MNIST_$K""_5"
+##Step 3 : 
   rm -rf "/home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/trained_models/mnist/mnist_$K""_5/weight_transfer/replication1"
+
+##Step 4 :
 python3 main.py @"trained_models/mnist/mnist_$K""_5/weight_transfer/opts.txt"
 
 done
 
-#mkdir /home/abhishek/Desktop/V2_MNIST_1_5 
 
-
-
-
-#rm -rf /home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/#trained_models/#mnist/mnist_1_5/weight_transfer/replication1
-
-
-#python3 main.py @trained_models/mnist/mnist_1_5/weight_transfer/opts.txt
-
-
-#mkdir /home/abhishek/Desktop/V2_MNIST_2_5 
-#rm -rf /home/abhishek/Desktop/ANU/comp_6470/adapted_deep_embeddings/#trained_models/mnist/mnist_2_5/weight_transfer/replication1
-#python3 main.py @trained_models/mnist/mnist_2_5/weight_transfer/opts.txt
 
 
 
